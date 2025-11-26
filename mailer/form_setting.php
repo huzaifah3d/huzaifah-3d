1709529f3f781c91082b61a8195a462d2e3d329c
Huzaifah 3D <huzaifah3d@gmail.com>
2025-11-26 17:58:19 +0200

    Add files via upload

---
+++ mailer/form_setting.php
@@ -0,0 +1,8 @@
+<?php
+	/*Form settings*/
+	$subj = "New message from the site 'Huzaifah 3D'"; //letter subject
+	$to = 'huzaifah3d@gmail.com'; // Enter Your E-mail
+	$from = 'admin@you-site-name.com'; // Admin e-mail
+	$fromName = 'Huzaifah 3D'; // Your company name
+	$charset = 'UTF-8';
+?>
